2716e3467f308de68a1b8373282888ea079cfd9e
Katarzyna Kawala <kat.kawala@gmail.com>
2021-10-12 21:20:07 +0200

    adding test checking for specific attributes

--- fields_test.go
+++ fields_test.go
@@ -7,9 +7,6 @@ import (
 )
 
 func TestFields(t *testing.T) {
-	type form struct {
-		Name string
-	}
 	tests := []struct {
 		strct interface{}
 		want  []field
@@ -112,6 +109,71 @@ func TestFields(t *testing.T) {
 			got := fields(tc.strct)
 			if !reflect.DeepEqual(got, tc.want) {
 				t.Errorf("fields() = %v; want %v", got, tc.want)
+			}
+		})
+	}
+}
+
+func TestFields_labels(t *testing.T) {
+	hasLabels := func(labels ...string) func(*testing.T, []field) {
+		return func(t *testing.T, fields []field) {
+			if len(fields) != len(labels) {
+				t.Fatalf("fields() len = %d; want %d", len(fields), len(labels))
+			}
+			for i := 0; i < len(fields); i++ {
+				if fields[i].Label != labels[i] {
+					t.Errorf("fields()[%d].Label = %s; want %s", i, fields[i].Label, labels[i])
+				}
+			}
+		}
+	}
+	hasValues := func(values ...interface{}) func(*testing.T, []field) {
+		return func(t *testing.T, fields []field) {
+			if len(fields) != len(values) {
+				t.Fatalf("fields() len = %d; want %d", len(fields), len(values))
+			}
+			for i := 0; i < len(fields); i++ {
+				if fields[i].Value != values[i] {
+					t.Errorf("fields()[%d].Value = %v; want %v", i, fields[i].Value, values[i])
+				}
+			}
+		}
+	}
+	check := func(checks ...func(*testing.T, []field)) []func(*testing.T, []field) {
+		return checks
+	}
+
+	tests := map[string]struct {
+		strct  interface{}
+		checks []func(*testing.T, []field)
+	}{
+		"No values": {
+			strct: struct {
+				Name string
+			}{},
+			checks: check(hasLabels("Name")),
+		},
+		"Multiple fields with values": {
+			strct: struct {
+				Name  string
+				Email string
+				Age   int
+			}{
+				Name:  "Kasia Kawala",
+				Email: "kat.kawala@gmail.com",
+				Age:   123,
+			},
+			checks: check(
+				hasLabels("Name", "Email", "Age"),
+				hasValues("Kasia Kawala", "kat.kawala@gmail.com", 123),
+			),
+		},
+	}
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			got := fields(tc.strct)
+			for _, check := range tc.checks {
+				check(t, got)
 			}
 		})
 	}
